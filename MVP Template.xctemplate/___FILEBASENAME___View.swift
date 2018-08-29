bee750dffd2aa99468bcb150056937c3db804abe
daferpi <daferpi@protonmail.ch>
2018-08-29 16:32:55 +0200

    initial commit

---
+++ MVP Template.xctemplate/___FILEBASENAME___View.swift
@@ -0,0 +1,15 @@
+//
+// ___FILENAME___
+// Created by ___FULLUSERNAME___ on ___DATE___
+//
+import Foundation
+
+protocol ___FILEBASENAMEASIDENTIFIER___ViewProtocol {
+	
+}
+
+class ___FILEBASENAMEASIDENTIFIER___View: ___FILEBASENAMEASIDENTIFIER___ViewProtocol {
+    
+}
+
+
